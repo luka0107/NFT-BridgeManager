7c419aaf25ce3b28f92f187a96443ccb41dc9c1f
selvanath-a <aselvanath@gmail.com>
2023-09-06 21:08:33 +0200

    Fixed dependency issues

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import "@nomiclabs/hardhat-ethers";
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.9",
+  defaultNetwork: "localhost",
+   networks: {
+      hardhat: {},
+   }
 };
 
 export default config;
